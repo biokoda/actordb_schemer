6f3c41a372ddfd389046352ec92a1b1cc404ccb4
Denis Justinek <denis.justinek@biokoda.com>
2015-11-13 21:18:52 +0100

    documented actordb_schemer_exec module

--- src/actordb_schemer_exec.erl
+++ src/actordb_schemer_exec.erl
@@ -10,6 +10,11 @@
 -define(STDOUT(X), ?STDOUT(X,[])).
 -define(STDOUT(X,Y), case actordb_schemer_cfg:silent() of false -> io:format(X,Y); true -> ok end).
 
+-spec check(ActorConfig :: term(), ProvidedSchema :: list()) -> {ok, list()}.
+%% @doc compares live schema from ActorDB with local schema provided by the callback module
+%%
+%%  ADBCfg should be compiled from actordb_client:config/0, actordb_client:config/1, actordb_client:config/2 or actordb_client:config/3 depending
+%%  on ActorDB configuration and connection
 check(ADBCfg, ProvidedSchema) ->
   ExpectedSchema = ProvidedSchema,
   CurrentSchema =  current_schema(ADBCfg),
@@ -19,6 +24,9 @@ check(ADBCfg, ProvidedSchema) ->
   end || {actor, Name, Changes} <- RequiredChanges],
   {ok, UpdateCommands}.
 
+-spec upgrade(ActorConfig :: term(), ProvidedSchema :: list()) -> {ok,list()} | {error,any()}.
+%% @doc upgrades live schema with queries that are result of actordb_schemer_exec:check/2
+%%
 upgrade({schema,_},_) ->
   ok;
 upgrade(ADBCfg, ProvidedSchema) ->
@@ -34,6 +42,9 @@ upgrade(ADBCfg, ProvidedSchema) ->
       {error, R}
   end.
 
+-spec current_schema(ActorConfig :: term()) -> list().
+%% @doc loads current schema from ActorDB to a list
+%%
 current_schema({schema,S}) ->
   S;
 current_schema(ADBCfg) ->
@@ -48,7 +59,10 @@ current_schema(ADBCfg) ->
     #adb_actor{name = Actor, tables = TableMeta}
 	end || Actor <- Actors].
 
-
+-spec upgrade_actors(TargetSchemaActors :: list(), CurrentSchemaActors :: list(), ChangeList :: list()) -> list().
+%% @doc takes current schema actors and checks if they need updating or creation
+%%  if an actor needs creation or updates {actor, Actor :: #adb_actor{}, TableChangeOrCreateQueries :: list()} term is added to changes list
+%%
 upgrade_actors([],_,Changes) ->
   Changes;
 upgrade_actors([Actor|L], CurSchema, Changes) when is_record(Actor, adb_actor) ->
@@ -69,7 +83,11 @@ upgrade_actors([Actor|L], CurSchema, Changes) when is_record(Actor, adb_actor) -
       upgrade_actors(L, CurSchema, [{actor, Actor, TCL}|Changes])
   end.
 
-
+-spec upgrade_tables(TargetActorTable :: list(), CurrentActor :: #adb_actor{}, TableChanges :: list()) -> list().
+%% @doc takes current actor tables and checks if they need updating or creation
+%%  if a table needs creation {create_table, Table :: #adb_table{}} term is added to changes list
+%%  if a table needs an update {alter_table, Table :: #adb_table{}, FieldChangeOrCreateList :: list()} term is added to changes list
+%%
 upgrade_tables([],_,Changes) ->
   Changes;
 upgrade_tables([Table|L], CurActorDef, Changes) when is_record(Table, adb_table) ->
@@ -91,6 +109,11 @@ upgrade_tables([Table|L], CurActorDef, Changes) when is_record(Table, adb_table)
       end
   end.
 
+-spec upgrade_fields(TargetFields :: list(), CurrentTable :: #adb_table{}, list()) -> list().
+%% @doc takes current table columns/fields and checks if they need updating or creation
+%%  if a field needs creation {add_column, Field :: #adb_field{}} term is added to changes list
+%%  if a field already exists it is left intact
+%%
 upgrade_fields([],_,Changes) ->
   Changes;
 upgrade_fields([Field|L], CurTableDef, Out) when is_record(Field, adb_field)->
@@ -99,11 +122,14 @@ upgrade_fields([Field|L], CurTableDef, Out) when is_record(Field, adb_field)->
       ?STDOUT("~s\t~s-- altering TABLE, adding COLUMN ~s (~p)~n",[?PARENT_MARK,?MARK(L),Field#adb_field.name,lists:flatten(io_lib:format("~p",[Field]))]),
       Command = {add_column, Field},
       upgrade_fields(L, CurTableDef, [Command|Out]);
-    CurFieldDef ->
+    _CurFieldDef ->
       ?STDOUT("~s\t~s-- COLUMN present: ~s (~p)~n",[?PARENT_MARK,?MARK(L),Field#adb_field.name,lists:flatten(io_lib:format("~p",[Field]))]),
       upgrade_fields(L, CurTableDef, Out)
   end.
 
+-spec extract_actor(Name :: binary(), ActorList :: list()) -> #adb_actor{} | undefined.
+%%  @doc looks up for an actor with a Name within a list of #adb_actor{} definitions
+%%
 extract_actor(_, []) ->
   undefined;
 extract_actor(Name, [Actor|R]) when is_record(Actor, adb_actor) ->
@@ -114,6 +140,9 @@ extract_actor(Name, [Actor|R]) when is_record(Actor, adb_actor) ->
       extract_actor(Name, R)
   end.
 
+-spec extract_table(Name :: binary(), TableList :: list()) -> #adb_table{} | undefined.
+%%  @doc looks up for a table with a Name within a list of #adb_table{} definitions
+%%
 extract_table(_, []) ->
   undefined;
 extract_table(Name, [Table|R]) when is_record(Table, adb_table) ->
@@ -124,6 +153,9 @@ extract_table(Name, [Table|R]) when is_record(Table, adb_table) ->
       extract_table(Name, R)
   end.
 
+-spec extract_field(Name :: binary(), FieldList :: list()) -> #adb_field{} | undefined.
+%%  @doc looks up for a field/column with a Name within a list of #adb_field{} definitions
+%%
 extract_field(_,[]) ->
   undefined;
 extract_field(Name,[Field|R]) ->
@@ -134,6 +166,10 @@ extract_field(Name,[Field|R]) ->
       extract_field(Name, R)
   end.
 
+-spec format_queries(Actor :: #adb_actor{}, ChangeList :: list(), ExpectedSchema :: list()) -> ChangeQueries :: binary().
+%%  @doc prepares update queries for an actor
+%%  if there are no updates required an empty binary is returned
+%%
 format_queries(Actor, Changes, ExpectedSchema) ->
   case format_queries0(Actor, Changes, ExpectedSchema, []) of
     [] ->
@@ -149,6 +185,9 @@ format_queries(Actor, Changes, ExpectedSchema) ->
       iolist_to_binary([ <<"actor ", ActorName/binary, KV/binary, "; ">> | R])
   end.
 
+-spec format_queries0(Actor :: #adb_actor{}, Changes :: list(), TargetSchema :: list(), InitialQueryResults :: list()) -> QueryResultList :: list().
+%% @doc transforms change terms to queries as binary strings
+%%
 format_queries0(_, [], _, Queries) ->
   Queries;
 format_queries0(Actor, [{alter_table, Table, Actions}|R], ExpectedSchema, Q) ->
@@ -180,6 +219,9 @@ format_queries0(Actor, [{create_table, Table}|R], ExpectedSchema, Q) ->
 format_queries0(Actor, Command, _, _) ->
   throw({update_error, Actor#adb_actor.name, Command}).
 
+-spec table_constraints(Constraints :: list()) -> QueryPart :: binary().
+%% @doc transforms constraint options to table query parts
+%%
 table_constraints(undefined) ->
   <<>>;
 table_constraints(Opts) ->
@@ -212,7 +254,9 @@ table_constraints(Opts) ->
     _ -> iolist_to_binary([<<",">>,join(C0," ")])
   end.
 
-
+-spec table_postcreate(Constraints :: list()) -> QueryPart :: binary().
+%% @doc transforms post create options to query parts
+%%
 table_postcreate(undefined) ->
   <<>>;
 table_postcreate(Opts) ->
@@ -233,6 +277,9 @@ table_postcreate(Opts) ->
     _ -> iolist_to_binary([<<" ">>,join(C0," ")])
   end.
 
+-spec get_constraints(Constraints :: list()) -> QueryPart :: binary().
+%% @doc transforms field constraint options to query field parts
+%%
 get_constraints(undefined) ->
   <<>>;
 get_constraints(L) ->
@@ -245,7 +292,7 @@ get_constraints(L) ->
   end || F <- L],
   iolist_to_binary([<<" ">>,join(Bin," ")]).
 
-
+%% @private
 join(L,Itm) ->
   join(L,Itm,[]).
 join([],_,Out) ->
